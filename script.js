09f893d0d3a61fb5929a9472464d28a840deafbb
Tyler <122329987+FrigidLife@users.noreply.github.com>
2023-08-19 16:15:08 -0400

    Add and center initial grid

---
+++ script.js
@@ -0,0 +1,15 @@
+const containerGrid = document.querySelector(".grid");
+
+let gridSize = 16;
+let numberOfBoxes = (gridSize ** 2);
+let gridPixels = gridSize * 10;
+let gridArea = (gridPixels ** 2);
+let boxFillPercentage = ((1 / gridSize) * 100);
+
+for (i = 1; i <= numberOfBoxes; i++) {
+    div = document.createElement('div');
+    div.style.flexBasis = `${boxFillPercentage}%`;
+    div.classList.add('gridItem');
+    containerGrid.appendChild(div);
+}
+
